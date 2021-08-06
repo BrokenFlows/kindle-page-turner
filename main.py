58bd1bacac7f44e2dc71cf52d07427bf002adf6d
BrokenFlows <cianfdowd@gmail.com>
2021-08-06 13:54:39 +0100

    adds servo motion on button press

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 from machine import Pin
-from utime import sleep
+from machine import PWM
+from utime import sleep, sleep_ms
 
 # buttons_on and _off will later decide if the pages moving backwards or forwards
 # for now the LED is used as a stand-in for the servo motors - before they arrive
-button_on = Pin(14,Pin.IN,Pin.PULL_DOWN)
-button_off = Pin(15,Pin.IN,Pin.PULL_DOWN)
+right_button = Pin(15,Pin.IN,Pin.PULL_DOWN)
+left_button = Pin(14,Pin.IN,Pin.PULL_DOWN)
+right_servo = PWM(Pin(17))
+left_servo = PWM(Pin(16))
 led = Pin(25, Pin.OUT)
 
-# light led immediately to show power on
+# blink led to show power on
 led.on()
+sleep(1)
+led.off()
 
-# constantly wait for input comma and toggle led accordingly
+# define the frequencies used for the servo motors
+frequency = 50
+right_servo.freq(frequency)
+left_servo.freq(frequency)
+
+# constantly wait for input, and toggle led accordingly
 while True:
-    if button_off.value():
-        led.off()
-    elif button_on.value():
-        led.on()
-
-    # sleep as a lazy form of debounce
-    sleep(0.15)
+    if right_button.value():
+        right_servo.duty_ns(500000) # move servo to screen 
+        sleep_ms(200) # wait for tap to register
+        right_servo.duty_ns(1300000) # re-home servo
+    elif left_button.value():
+        left_servo.duty_ns(2500000) # move servo to screen
+        sleep_ms(200) # wait for tap to register
+        left_servo.duty_ns(1600000) # re-home servo
 
