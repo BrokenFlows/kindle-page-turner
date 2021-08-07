5d9b6abd0f2ba660ff3ed2dde5b9702c84fdc4af
BrokenFlows <cianfdowd@gmail.com>
2021-08-07 11:24:15 +0100

    moves servo motion to functions

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ from machine import Pin
 from machine import PWM
 from utime import sleep, sleep_ms
 
-# buttons_on and _off will later decide if the pages moving backwards or forwards
-# for now the LED is used as a stand-in for the servo motors - before they arrive
+# set pins for button and led
 right_button = Pin(15,Pin.IN,Pin.PULL_DOWN)
 left_button = Pin(14,Pin.IN,Pin.PULL_DOWN)
-right_servo = PWM(Pin(17))
-left_servo = PWM(Pin(16))
 led = Pin(25, Pin.OUT)
 
 # blink led to show power on
@@ -20,16 +17,26 @@ frequency = 50
 right_servo.freq(frequency)
 left_servo.freq(frequency)
 
-# constantly wait for input, and toggle led accordingly
+def right_tap(screen_pos, home_pos, wait_ms):
+    right_servo = PWM(Pin(17)) # right servo pin
+
+    right_servo.duty_ns(screen_pos) # move servo to screen 
+    sleep_ms(wait_ms) # wait for tap to register
+    right_servo.duty_ns(home_pos) # re-home servo
+    sleep_ms(wait_ms) # wait for re-home
+
+def left_tap(screen_pos, home_pos, wait_ms):
+    left_servo = PWM(Pin(16)) # left servo pin
+
+    left_servo.duty_ns(screen_pos) # move servo to screen
+    sleep_ms(wait_ms) # wait for tap to register
+    left_servo.duty_ns(home_pos) # re-home servo
+    sleep_ms(wait_ms) # wait for re-home
+
+# constantly wait for input, and tap screen accordingly
 while True:
     if right_button.value():
-        right_servo.duty_ns(500000) # move servo to screen 
-        sleep_ms(200) # wait for tap to register
-        right_servo.duty_ns(1300000) # re-home servo
-        sleep_ms(200) # wait for re-home
+        right_tap(500000, 1300000, 200) # tap right side on right button press
     elif left_button.value():
-        left_servo.duty_ns(2500000) # move servo to screen
-        sleep_ms(200) # wait for tap to register
-        left_servo.duty_ns(1600000) # re-home servo
-        sleep_ms(200) # wait for re-home
+        left_tap(2500000, 1600000, 200) # tap left side on left button press
 
