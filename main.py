1249939fbc7110c9e4205b1f65d30dc3a242ed53
BrokenFlows <cianfdowd@gmail.com>
2021-08-10 16:56:38 +0100

    changes tap timing and improves readability

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ left_servo_pin = 16
 led = Pin(25, Pin.OUT)
 frequency = 50
 
+# servo durations for target positions
+home_right = 1300000
+screen_right = 500000
+home_left = 1600000
+screen_left = 2500000
+
+# servo wait durations in ms
+wait_dur = 250
+
 # blink led to show power on
 led.on()
 sleep(1)
@@ -29,7 +38,7 @@ def screen_tap(servo_pin, frequency, screen_pos, home_pos, wait_ms):
 # constantly wait for input, and tap screen accordingly
 while True:
     if right_button.value():
-        screen_tap(right_servo_pin, frequency, 500000, 1300000, 200)
+        screen_tap(right_servo_pin, frequency, screen_right, home_right, wait_dur)
     elif left_button.value():
-        screen_tap(left_servo_pin, frequency, 2500000, 1600000, 200)
+        screen_tap(left_servo_pin, frequency, screen_left, home_left, wait_dur)
 
